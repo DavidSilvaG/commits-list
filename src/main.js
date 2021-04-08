60f00a3a12245990ff9bad5fabf90e64d0259c14
DavidSilvaG <dsg1089@gmail.com>
2021-04-08 12:32:07 -0500

    creating empty page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import VerticalTimeline from 'vue-vertical-timeline';
+Vue.use(VerticalTimeline);
 
 Vue.config.productionTip = false
 
